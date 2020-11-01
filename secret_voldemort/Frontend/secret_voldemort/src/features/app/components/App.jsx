f356f4aefd04aa822fd3c42ed44ceab5c4532e2b
Miguel Angel Rodenas Balestrini <miguel.rodenas@mi.unc.edu.ar>
2020-11-01 20:57:23 -0300

    pre merge

--- secret_voldemort/Frontend/secret_voldemort/src/features/app/components/App.jsx
+++ secret_voldemort/Frontend/secret_voldemort/src/features/app/components/App.jsx
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
-import CreateGameContainer from '../../createGameForm/containers/container'
-import { HashRouter as Router, Redirect, Route } from "react-router-dom";
+
 
 
 class App extends Component {
     render() {
-        return (
-            <Router>
-                <Route exact path="/">
-                    <Redirect to="/createGame" />
-                </Route>
-                <Route exact path='/createGame' component={CreateGameContainer} />
-            </Router>
-        )
+       
     }
 }
 

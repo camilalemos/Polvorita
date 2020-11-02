418daed058a612bb5d6fa12b8ff5135a9847fcb7
Francisco griguol <frangriguol13@gmail.com>
2020-11-01 21:23:32 -0300

    Add token

--- secret_voldemort/Frontend/secret_voldemort/src/features/app/containers/AppContainer.js
+++ secret_voldemort/Frontend/secret_voldemort/src/features/app/containers/AppContainer.js
@@ -3,13 +3,17 @@ import { connect } from 'react-redux';
 
 import App from '../components/App';
 
+import { getUserData } from '../../login/actions/LoginAction';
+
 function mapStateToProps(state) {
     return {
+        statusLogin: state.login.statusLogin
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
+        getUserData
     }, dispatch);
 };
 

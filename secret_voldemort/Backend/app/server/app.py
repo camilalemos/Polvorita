6c472247d6c45b3e1c3c2bf3c06f6efcfdde4bca
Nibblex <50432470+Nibblex@users.noreply.github.com>
2020-11-03 01:22:06 -0300

    Update app.py

--- secret_voldemort/Backend/app/server/app.py
+++ secret_voldemort/Backend/app/server/app.py
@@ -11,7 +11,9 @@ from pydantic import BaseModel, EmailStr
 
 from .authentication import *
 from .wsmanager import *
-from .models import *
+from .user import *
+from .game import *
+from .enums import *
 from .db import db
 
 
@@ -33,40 +35,43 @@ app.add_middleware(
 
 
 #JOIN GAME
-@app.put("/game/")
-async def join_game(game_name: str = Form(...),
-                    player_name: str = Form(...),
-                    password: Optional[str] = Form(None),
+@app.put("/game/", response_model=Game)
+async def join_game(game_name: str = Form(..., min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$")),
+                    player_name: str = Form(..., min_length=3, max_length=10, regex="^[A-Z_a-z0-9]*$")),
+                    password: Optional[str] = Form(None, min_length=5, max_length=10, regex="^[A-Za-z0-9]*$")),
                     user: User = Depends(get_current_active_user)):
     game = manager.games.get(game_name)
     if not game:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
+    elif game.password != password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
+    elif player_name in game.players:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Player name already exist in this game")
     elif len(game.players) == game.num_players:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game full")
     else:
-        password_match = game.password == password
-        if player_name in game.players:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Player name already exist in this game")
-        elif not password_match:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
-        else:
-            player = Player(player_name=player_name)
-            game.players[player_name] = player
+        player = Player(name=player_name, game_name=game_name)
+        game.players[player_name] = player
+        manager.players[player_name] = player
 
     return game
 
 #START GAME
-@app.put("/game/{game_name}")
-async def start_game(game_name: str, player_name: str, user: User = Depends(get_current_active_user)):
+@app.put("/game/start/{player_name}", response_model=Game)
+async def start_game(player_name: str, user: User = Depends(get_current_active_user)):
+    game_name = manager.players.get(player_name).game_name
     game = manager.games.get(game_name)
-    if game:
-        if game.owner_name == player_name:
-            game.start()
-        else:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only game owner can start the game")
-    else:
+    if not game:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
-    
+    elif game.game_status != 'CREATED':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game already started")
+    elif game.owner_name != player_name:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only game owner can start the game")
+    elif len(game.players) != game.num_players:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough players")
+    else:
+        game.start()
+
     return game
 
 

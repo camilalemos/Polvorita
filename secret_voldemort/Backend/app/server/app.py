4bf4114174bb026f57ba9ab1b33db2773a6281b2
camilalemos <clemos996@famaf.unc.edu.ar>
2020-11-03 04:00:14 -0300

    Update app.py

--- secret_voldemort/Backend/app/server/app.py
+++ secret_voldemort/Backend/app/server/app.py
@@ -128,24 +128,3 @@ async def websocket_game(websocket: WebSocket, game_name: str):
 
     except Exception:
         manager.disconnect_game(websocket, game_name)
-        
-#ENACT PROCLAMATION
-@app.put("/game/proclamation/{player_name}", response_model=Game)
-async def enact_proclamation(player_name: str, loyalty: Loyalty, user: User = Depends(get_current_active_user)):
-    if loyalty not in ['PHOENIX_ORDER', 'DEATH_EATERS']:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid loyalty")
-
-    game_name = manager.players.get(player_name).game_name
-    game = manager.games.get(game_name)
-    if not game:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
-    elif game.game_status != 'STARTED':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
-    elif game.players.get(player_name).player_status != 'HEADMASTER':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only headmaster can enact proclamation")
-    else:
-        game.board.enact_proclamation(loyalty)
-        game.finish()
-    
-    return game
-

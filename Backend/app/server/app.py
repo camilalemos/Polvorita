c995cb2052357382f0457a531a489c20bab47d82
mmeaz850 <31185317+mmeaz850@users.noreply.github.com>
2020-11-11 15:01:30 -0300

    modifying endpoints

--- Backend/app/server/app.py
+++ Backend/app/server/app.py
@@ -5,7 +5,7 @@ from passlib.context import CryptContext
 from fastapi import FastAPI, Depends, Form, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
-from pony.orm import db_session, exists,select
+from pony.orm import db_session, exists
 from pydantic import EmailStr
 
 from .authentication import *
@@ -49,7 +49,7 @@ async def register_user(username: str = Form(..., min_length=5, max_length=20, r
         return user_id
 
 #LOGIN
-@app.post("/token/", status_code=201, response_model=Token)
+@app.post("/login/", status_code=201, response_model=Token)
 async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
     user = authenticate_user(db.User, form_data.username, form_data.password)
     if not user:
@@ -62,103 +62,137 @@ async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
+
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+#CHANGE PROFILE AND PASSWORD
+@app.put("/user/", response_model=User)
+async def change_profile(email: Optional[EmailStr] = Form(None),
+                         username: Optional[str] = Form(None, min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$"),
+                         full_name: Optional[str] = Form(None, min_length=5, max_length=30, regex="^[A-Z a-z0-9]*$"),
+                         new_password: Optional[str] = Form(None, min_length=8, max_length=20, regex="^[A-Za-z0-9]*$"),
+                         password: str = Form(..., min_length=8, max_length=20),
+                         user: User = Depends(get_current_active_user)):
+    with db_session:
+        if username and db.User.exists(username=username):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Username already exist")    
+        elif email and db.User.exists(email=email):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="E-mail already exist")
+
+        if not verify_password(password, user.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect credentials")
+
+        if new_password:
+            new_password = get_password_hash(new_password)
+
+        updated_user = get(p for p in db.User if p.id == user.id)
+        updated_user = updated_user.update(email, username, full_name, new_password)
+
+    return updated_user.to_dict()
 
 #CREATE GAME
 @app.post("/game/", status_code=201, response_model=Game)
 async def create_game(game_name: str = Form(..., min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$"),
-                      player_name: str = Form(..., min_length=3, max_length=10, regex="^[A-Z_a-z0-9]*$"),
+                      player_name: str = Form(..., min_length=3, max_length=15, regex="^[A-Z_a-z0-9]*$"),
                       password: Optional[str] = Form(None, min_length=5, max_length=10, regex="^[A-Za-z0-9]*$"),
                       user: User = Depends(get_current_active_user)):
     if game_name in manager.games:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game name already exist")
     else:
-        game = Game(name=game_name, owner_name=player_name, owner_username=user.username, password=password)
-        player = Player(name=player_name, game_name=game_name)
-        game.players[player_name] = player
+        game = Game(name=game_name, password=password)
+        game.create_player(player_name, user.username)
         manager.create_game(game)
 
     return game
 
-#JOIN GAME
-@app.put("/game/", response_model=Game)
-async def join_game(game_name: str = Form(..., min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$"),
-                    player_name: str = Form(..., min_length=3, max_length=10, regex="^[A-Z_a-z0-9]*$"),
-                    password: Optional[str] = Form(None, min_length=5, max_length=10, regex="^[A-Za-z0-9]*$"),
-                    user: User = Depends(get_current_active_user)):
+def get_game(game_name: str, user: User = Depends(get_current_active_user)):
     game = manager.games.get(game_name)
     if not game:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
-    elif game.owner_username == user.username:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="A user cannot enter his own game")
-    elif game.password != password:
+
+    return {"game": game, "user": user}
+
+#JOIN GAME
+@app.put("/game/", response_model=Game)
+async def join_game(player_name: str = Form(..., min_length=3, max_length=15, regex="^[A-Z_a-z0-9]*$"),
+                    password: Optional[str] = Form(None, min_length=5, max_length=10, regex="^[A-Za-z0-9]*$"),
+                    params = Depends(get_game)):
+    game = params["game"]
+    username = params["user"].username
+    if game.exist(username):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="A user cannot enter a game twice")
+    elif password != game.password:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
     elif player_name in game.players:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Player name already exist in this game")
-    elif len(game.players) == game.max_players:
+    elif game.num_players == game.max_players:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game full")
     else:
-        player = Player(name=player_name, game_name=game_name)
-        game.players[player_name] = player
+        game.create_player(player_name, username)
 
     return game
-
-def get_game(game_name: str, player_name: str, user: User = Depends(get_current_active_user)):
-    game = manager.games.get(game_name)
-    if not game:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
-    elif not game.players.get(player_name):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
-
-    return game
-
 
 #START GAME
-@app.put("/game/start/{player_name}", response_model=Game)
-async def start_game(player_name: str, game: Game = Depends(get_game)):
+@app.put("/game/start/", response_model=Game)
+async def start_game(params = Depends(get_game)):
+    game = params["game"]
     if game.status != 'CREATED':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game already started")
-    elif game.owner_name != player_name:
+    elif params["user"].username != game.owner():
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only game owner can start the game")
-    elif len(game.players) != game.max_players:
+    elif game.num_players < game.min_players:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough players")
     else:
         game.start()
 
     return game
 
-#ENACT PROCLAMATION
-@app.put("/game/proclamation/enact/{player_name}", response_model=Game)
-async def enact_proclamation(player_name:str, loyalty: Loyalty, game: Game = Depends(get_game)):
+def check_params(player_name: str, params = Depends(get_game)):
+    game = params["game"]
     if game.status != 'STARTED':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
-    elif game.players.get(player_name).status != 'HEADMASTER':
+    player = game.players.get(player_name)
+    if not player:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+    elif not player.is_alive:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Player is not alive")
+
+    return {"game": game, "player": player}
+
+#ENACT PROCLAMATION
+@app.put("/game/proclamation/enact/", response_model=Game)
+async def enact_proclamation(loyalty: Loyalty, params = Depends(check_params)):
+    game = params["game"]
+    if params["player"].status != 'HEADMASTER':
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only headmaster can enact a proclamation")
     else:
-        game.board.enact_proclamation(loyalty)
+        game.proclamations.enact_proclamation(loyalty)
         game.finish(manager)
 
     return game
 
 #DISCARD PROCLAMATION
-@app.put("/game/proclamation/discard/{player_name}", response_model=Game)
-async def discard_proclamation(player_name:str, loyalty: Loyalty, game: Game = Depends(get_game)):
-    if game.status != 'STARTED':
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
-    elif game.players.get(player_name).status not in ['MINISTER', 'HEADMASTER']:
+@app.put("/game/proclamation/discard/", response_model=Game)
+async def discard_proclamation(loyalty: Loyalty, params = Depends(check_params)):
+    game = params["game"]
+    if params["player"].status not in ['MINISTER', 'HEADMASTER']:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only minister or headmaster can discard a proclamation")
     else:
-        game.board.discard_proclamation(loyalty)
+        game.proclamations.discard_proclamation(loyalty)
 
     return game
 
+
+
 #CHOOSE DIRECTOR
 
-@app.put("/game/director/election/{player_name}", response_model=Game)
-async def choose_director(player_name:str ,director_candidate:str ,game: Game = Depends(get_game)):
+@app.put("/game/director/election/", response_model=Game)
+async def choose_director(director_candidate:str ,params = Depends(check_params)):
+    game = params["game"]
     if game.status_game('STARTED'):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
-    elif player_name != game.elections.minister_candidate:
+    elif params["player"].name != game.elections.minister_candidate:
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only the minister can select a candidate for director")
     elif game.elections.last_minister == director_candidate or game.elections.last_headmaster == director_candidate:
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="the candidate has been a minister or director")
@@ -168,21 +202,23 @@ async def choose_director(player_name:str ,director_candidate:str ,game: Game = 
 
 #VOTE
 
-@app.put("/game/vote/{player_name}", response_model=Game)
-async def player_vote(player_name:str, vote:Vote, game: Game = Depends(get_game)):
+@app.put("/game/vote/", response_model=Game)
+async def player_vote(vote:Vote, params = Depends(check_params)):
+    game = params["game"]
     if game.status_game('STARTED'):
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
-    elif player_name in game.elections.votes.keys():
+    elif params["player"].name in game.elections.votes.keys():
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="the player has already voted")
     else:
-        game.elections.votation(player_name,vote)
+        game.elections.votation(params["player"].name,vote)
     return game
 
 
 #SHOW ELECTION RESULTS
 
 @app.put("/game/vote/results/")
-async def show_election_results(game: Game = Depends(get_game)):
+async def show_election_results(params = Depends(check_params)):
+    game = params["game"]
     if game.status_game('STARTED'):
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
     elif game.elections.check_votes(5):
@@ -200,7 +236,8 @@ async def show_election_results(game: Game = Depends(get_game)):
 # shift change logic
 
 @app.put("/game/change/turn/")
-async def shift_change_logic(game: Game = Depends(get_game)):
+async def shift_change_logic(params = Depends(check_params)):
+      game = params["game"]
       if  game.status_game('STARTED'):
          raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
       else:

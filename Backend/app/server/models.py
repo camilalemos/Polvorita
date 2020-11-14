c01d70f6034ff20b0c8ca41e07ee4a7dc0a5d4c6
mmeaz850 <31185317+mmeaz850@users.noreply.github.com>
2020-11-14 18:37:52 -0300

    modifications in models.py

--- Backend/app/server/models.py
+++ Backend/app/server/models.py
@@ -27,40 +27,40 @@ class Player(BaseModel):
     is_alive: bool = True
     role: Role = None
     loyalty: Loyalty = None
-    status: PlayerStatus = 'COMMON'
 
     def kill(self):
         self.is_alive = True
-    
+
 class Elections(BaseModel):
     minister_candidate: str = None
     headmaster_candidate: str = None
-    last_minister: str = None #
-    last_headmaster: str = None #
-    governments_rejected: int = 0 #
+    minister: str = None
+    headmaster: str = None
+    rejected: int = 0
     votes: Dict[str, Vote] = {}
 
-    def nominate(self, status: PlayerStatus, player_name: str):
-        if status not in ['MINISTER', 'HEADMASTER']:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid nomination")
-        elif status == 'MINISTER':
-            self.minister_candidate = player_name
-        elif status == 'HEADMASTER':
-            self.headmaster_candidate = player_name
-    def last_minister_headmaster(self,minister,headmaster):#
-        self.last_minister   = minister
-        self.last_headmaster = headmaster
-    def votation(self,player_name,vote):#
+    def nominate(self, nomination: Nomination, candidate: str):
+        if nomination == 'MINISTER':
+            self.minister_candidate = candidate
+        elif nomination == 'HEADMASTER':
+            self.headmaster_candidate = candidate
+
+    def vote(self, player_name, vote):
         self.votes[player_name] = vote
-    def vote_lumos(self):#
-        return  sum(map(("LUMOS").__eq__, self.votes.values()))
-    def vote_nox(self):#
-        return  sum(map(("NOX").__eq__, self.votes.values()))
-    def check_votes(self,total_votes):#
-        return (sum(map(len, self.votes.values())) < total_votes)
-    def rejected(self):#
-        self.governments_rejected += 1
-    
+
+    def results(self):
+        lumos_votes = sum(map(('LUMOS').__eq__, self.votes.values()))
+        nox_votes = sum(map(('NOX').__eq__, self.votes.values()))
+        if lumos_votes < nox_votes:
+            self.rejected += 1
+            result = 'NOX'
+        else:
+            self.minister = self.minister_candidate
+            self.headmaster = self.headmaster_candidate
+            result = 'LUMOS'
+
+        self.votes.clear()
+        return result
 
 class Proclamations(BaseModel):
     proclamations: List[Loyalty] = []
@@ -68,7 +68,7 @@ class Proclamations(BaseModel):
     PO_enacted_proclamations: int = 0
     DE_enacted_proclamations: int = 0
 
-    def init_proclamations(self):
+    def init(self):
         for i in range(6):
             self.proclamations.append('PHOENIX_ORDER')
         for i in range(11):
@@ -83,13 +83,13 @@ class Proclamations(BaseModel):
             random.shuffle(self.proclamations)
             self.get_3proclamations()
 
-    def enact_proclamation(self, loyalty: Loyalty):
+    def enact(self, loyalty: Loyalty):
         if loyalty == 'PHOENIX_ORDER':
             self.PO_enacted_proclamations += 1
         elif loyalty == 'DEATH_EATERS':
             self.DE_enacted_proclamations += 1
 
-    def discard_proclamation(self, loyalty: Loyalty):
+    def discard(self, loyalty: Loyalty):
         if loyalty == 'DEATH_EATERS':
             self.PO_discarded_proclamations += 1
         elif loyalty == 'DEATH_EATERS':
@@ -102,16 +102,19 @@ class Game(BaseModel):
     winner: Loyalty = None
     min_players: int = 5
     max_players: int = 5
-    num_players: int = 0   
-    players: Dict[str, Player] = {}   
+    num_players: int = 0
+    players: Dict[str, Player] = {}
     proclamations: Proclamations = None
     elections: Elections = None
     spells: List[Spell] = []
     chat: List[str] = []
-    
+
     def exist(self, username: str):
         users = [player.user_name for player in self.players.values()]
         return username in users
+
+    def owner(self):
+        return list(self.players.values())[0].user_name
 
     def create_player(self, player_name: str, username: str):
         self.players[player_name] = Player(name=player_name, user_name=username)
@@ -120,7 +123,7 @@ class Game(BaseModel):
     def delete_player(self, player_name: str):
         self.players.pop(player_name)
         self.num_players -= 1
-    
+
     def assign_roles(self):
         to_assign_phoenix_order = [player for player in self.players.values() if player.loyalty == 'PHOENIX_ORDER']
         to_assign_death_eaters = [player for player in self.players.values() if player.loyalty == 'DEATH_EATERS']
@@ -140,37 +143,25 @@ class Game(BaseModel):
             to_assign.pop().loyalty = 'PHOENIX_ORDER'
         for i in range(num_death_eaters):
             to_assign.pop().loyalty = 'DEATH_EATERS'
-   
-    def owner(self):
-        return list(self.players.values())[0].user_name
 
     def start(self):
         self.status = 'STARTED'
         self.proclamations = Proclamations()
-        self.proclamations.init_proclamations()
+        self.proclamations.init()
         self.assign_loyalties()
         self.assign_roles()
         self.elections = Elections()
         first_candidate = random.choice(list(self.players.keys()))
         self.elections.nominate('MINISTER', first_candidate)
 
-    def finish(self, manager):
-        if self.status == 'STARTED' and self.proclamations.PO_enacted_proclamations == 5:
+    def get_winner(self):
+        if self.proclamations.PO_enacted_proclamations == 5:
             self.winner = 'PHOENIX_ORDER'
-            self.status = 'FINISHED'
-            manager.delete_game(self.name)
-        elif self.status == 'STARTED' and self.proclamations.DE_enacted_proclamations == 6:
+        elif self.proclamations.DE_enacted_proclamations == 6:
             self.winner = 'DEATH_EATERS'
-            self.status = 'FINISHED'
-            manager.delete_game(self.name)
-    def new_minister_director(self,minister,headmaster):#
-        self.players.get(minister).status = 'MINISTER'
-        self.players.get(headmaster).status = 'HEADMASTER'
-    def election_results(self):
-        if self.elections.vote_lumos() < self.elections.vote_nox():
-            self.elections.rejected()
-            return 'NOX'
-        else:
-            self.elections.last_minister_headmaster(self.elections.minister_candidate,self.elections.headmaster_candidate)
-            self.new_minister_director(self.elections.minister_candidate,self.elections.headmaster_candidate)                
-            return 'LUMUS'
+
+        return self.winner
+
+    def finish(self, manager):
+        self.status = 'FINISHED'
+        manager.delete_game(self.name)

13d59a48263787a30ef7c50c82a298201baf269b
Tomas Uribe <tomas@DESKTOP-MM47BAT.localdomain>
2020-11-15 00:07:42 -0300

    Snackbar and Proclamation

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store/index'
-import Board from '../src/features/board/containers/BoardContainers.js'
-import Game from '../src/features/board/components/game'
+import Game from '../src/features/game/components/game'
 
 const store = configureStore()
 

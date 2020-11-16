dd2e88e0be57811c4f27e82250c61fd97d2d0a0f
Miguel Angel Rodenas Balestrini <miguel.rodenas@mi.unc.edu.ar>
2020-11-16 11:02:23 -0300

    tittle added

--- Frontend/src/features/changeProfile/components/ChangeProfile.jsx
+++ Frontend/src/features/changeProfile/components/ChangeProfile.jsx
@@ -110,92 +110,94 @@ const ChangeProfile = function ({ userInfo, getUserInfo, status, errorMsg, open,
             aria-labelledby="alert-dialog-slide-title"
             aria-describedby="alert-dialog-slide-description"
         >
-            
-            <List component="nav"  aria-label="contacts">
-                <ListItem >
-                    <ListItemText primary="Username" />
-                    <TextField
-                        value={userName}   
-                        error={errorUserName}
-                        id="Username" 
-                        label={userNameLabel} 
-                        variant="filled" 
-                        //style={{ marginBottom: 40, minWidth:300 }}
-                        onChange={(value) => (setUserName(value.target.value), setErrorUserName(false))}
-                        onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
-                    />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary="E-mail" />
-                    <TextField 
-                        value={email}  
-                        error={errorEmail} 
-                        id="E-mail" 
-                        label={emailLabel} 
-                        variant="filled" 
-                        onChange={(value) => (setEmail(value.target.value), setErrorEmail(false))}
-                        onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
-                    />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary="Fullname" />
-                    <TextField 
-                        value={fullName} 
-                        error={errorFullName}
-                        id="Full Name" 
-                        label={fullNameLabel} 
-                        variant="filled"
-                       // style={{ marginBottom: 40, minWidth:300 }}
-                        onChange={(value) => (setFullName(value.target.value), setErrorFullName(false))}
-                        onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
-                      
-                    />
-                </ListItem>
-                <ListItem>
-                    <ListItemText primary="Change Password" />
-                    <TextField 
-                        value={newPassword} 
-                        error={errorNewPassword}
-                        id="Change Password" 
-                        label="New password" 
-                        type='password'
-                        variant="filled" 
-                        onChange={(value) => (handleChangePassword(value))}
-                        onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
-                    />
-                </ListItem>
-                <div>
-                    {renderConfirmPassword 
-                    ?
+            <div style={{ display:'flex', justifyContent:'center', alignItems:'center', flexDirection:'column', padding: 40}} id='fondo'>
+                <a style={{ textAlign: 'center', fontSize:40,  marginBottom:60}} id="alert-dialog-slide-title">{"MY PROFILE"}</a>
+                <List component="nav"  aria-label="contacts">
+                    <ListItem >
+                        <ListItemText primary="Username" />
+                        <TextField
+                            value={userName}   
+                            error={errorUserName}
+                            id="Username" 
+                            label={userNameLabel} 
+                            variant="filled" 
+                            //style={{ marginBottom: 40, minWidth:300 }}
+                            onChange={(value) => (setUserName(value.target.value), setErrorUserName(false))}
+                            onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
+                        />
+                    </ListItem>
                     <ListItem>
-                        <ListItemText primary="Confirm New Password" />
+                        <ListItemText primary="E-mail" />
                         <TextField 
-                            value={confirmPassword}
-                            error={errorConfirmPassword}
-                            id="Confirm New Password" 
-                            required
-                            label="New Password" 
+                            value={email}  
+                            error={errorEmail} 
+                            id="E-mail" 
+                            label={emailLabel} 
+                            variant="filled" 
+                            onChange={(value) => (setEmail(value.target.value), setErrorEmail(false))}
+                            onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="Fullname" />
+                        <TextField 
+                            value={fullName} 
+                            error={errorFullName}
+                            id="Full Name" 
+                            label={fullNameLabel} 
+                            variant="filled"
+                        // style={{ marginBottom: 40, minWidth:300 }}
+                            onChange={(value) => (setFullName(value.target.value), setErrorFullName(false))}
+                            onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
+                        
+                        />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="Change Password" />
+                        <TextField 
+                            value={newPassword} 
+                            error={errorNewPassword}
+                            id="Change Password" 
+                            label="New password" 
                             type='password'
                             variant="filled" 
-                            onChange={(value) => (setConfirmPassword(value.target.value), setErrorConfirmPassword(false))}
+                            onChange={(value) => (handleChangePassword(value))}
                             onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
-                        /> 
+                        />
                     </ListItem>
-                    :
-                    <div></div>   
-                    }
+                    <div>
+                        {renderConfirmPassword 
+                        ?
+                        <ListItem>
+                            <ListItemText primary="Confirm New Password" />
+                            <TextField 
+                                value={confirmPassword}
+                                error={errorConfirmPassword}
+                                id="Confirm New Password" 
+                                required
+                                label="New Password" 
+                                type='password'
+                                variant="filled" 
+                                onChange={(value) => (setConfirmPassword(value.target.value), setErrorConfirmPassword(false))}
+                                onKeyPress={(e) => {if (e.key === 'Enter') handleSave()}}
+                            /> 
+                        </ListItem>
+                        :
+                        <div></div>   
+                        }
+                    </div>
+                </List>
+                <PopUp sendNewInfo={handleUpdateInfo} open={openModal} 
+                password={password} errorPassword={errorPassword} setPassword={(value) => setPassword(value)} 
+                setErrorPassword={(value) => setErrorPassword(value)} onClose={() => setOpenModal(false)}/>
+                <div style={{ flexDirection:'row'}}>
+                            <Button size='small' onClick={onClose} variant="outlined" color="primary" style={{ borderRadius:4, width:120 }}>
+                                Cancel
+                            </Button>
+                            <Button  color="primary" onClick={handleSave}>
+                                Save
+                            </Button>
                 </div>
-            </List>
-            <PopUp sendNewInfo={handleUpdateInfo} open={openModal} 
-            password={password} errorPassword={errorPassword} setPassword={(value) => setPassword(value)} 
-            setErrorPassword={(value) => setErrorPassword(value)} onClose={() => setOpenModal(false)}/>
-            <div style={{ flexDirection:'row'}}>
-                        <Button size='small' onClick={onClose} variant="outlined" color="primary" style={{ borderRadius:4, width:120 }}>
-                            Cancel
-                        </Button>
-                        <Button  color="primary" onClick={handleSave}>
-                             Save
-                        </Button>
             </div>
         </Dialog>
         </div>  

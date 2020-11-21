4fa741cd9ff060d1ee0622b6ba3f84f0f5448703
mmeaz850 <31185317+mmeaz850@users.noreply.github.com>
2020-11-21 10:01:41 -0300

    add files

---
+++ Backend/app/server/app.py
@@ -0,0 +1,254 @@
+from fastapi import FastAPI, Form
+from fastapi.middleware.cors import CORSMiddleware
+import asyncio
+
+from .authentication import *
+from .wsmanager import *
+from .models import *
+from .enums import *
+from .db import db
+
+app = FastAPI()
+manager = ConnectionManager()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#REGISTER
+@app.post("/user/", status_code=201)
+def register_user(email: EmailStr = Form(...),
+                  username: str = Form(..., min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$"),
+                  password: str = Form(..., min_length=8, max_length=20, regex="^[A-Za-z0-9]*$")):
+    with db_session:
+        hashed_password = get_password_hash(password)
+        user = User(username=username, email=email, password=hashed_password)
+
+        try:
+            user_id = db.User(**user.dict()).to_dict()['id']
+        except Exception:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Username or E-mail already exist")
+
+        return user_id
+
+#LOGIN
+@app.post("/login/", status_code=201, response_model=Token)
+def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db.User, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+#CHANGE PROFILE AND PASSWORD
+@app.put("/user/", response_model=User)
+def change_profile(email: Optional[EmailStr] = Form(None),
+                   username: Optional[str] = Form(None, min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$"),
+                   full_name: Optional[str] = Form(None, min_length=5, max_length=30, regex="^[A-Z a-z0-9]*$"),
+                   new_password: Optional[str] = Form(None, min_length=8, max_length=20, regex="^[A-Za-z0-9]*$"),
+                   password: str = Form(...),
+                   user: User = Depends(get_current_active_user)):
+    with db_session:
+        if username or email or full_name or new_password:
+            if not verify_password(password, user.password):
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect credentials")
+            elif username and db.User.exists(username=username):
+                raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Username already exist")
+            elif email and db.User.exists(email=email):
+                raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="E-mail already exist")
+
+        updated_user = db.User[user.id]
+        hashed_password = get_password_hash(new_password)
+        updated_user = updated_user.update(email, username, full_name, hashed_password)
+        return updated_user.to_dict()
+
+#CREATE GAME
+@app.post("/game/", status_code=201, response_model=Game)
+def create_game(game_name: str = Form(..., min_length=5, max_length=20, regex="^[A-Z_a-z0-9]*$"),
+                player_name: str = Form(..., min_length=3, max_length=15, regex="^[A-Z_a-z0-9]*$"),
+                password: Optional[str] = Form(None, min_length=5, max_length=10, regex="^[A-Za-z0-9]*$"),
+                user: User = Depends(get_current_active_user)):
+    if game_name in manager.games:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game name already exist")
+
+    game = Game(name=game_name, owner=user.username, password=password)
+    game.create_player(player_name, user.username)
+    manager.create_game(game)
+    return game
+
+def get_game(game_name: str, user: User = Depends(get_current_active_user)):
+    game = manager.games.get(game_name)
+    if not game:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Game not found")
+
+    return {"game": game, "user": user}
+
+#JOIN GAME
+@app.put("/game/", response_model=Game)
+def join_game(player_name: str = Form(..., min_length=3, max_length=15, regex="^[A-Z_a-z0-9]*$"),
+              password: Optional[str] = Form(None),
+              params = Depends(get_game)):
+    game = params["game"]
+    username = params["user"].username
+    if game.exist(username):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="A user cannot enter a game twice")
+    elif password != game.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
+    elif player_name in game.players:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Player name already exist in this game")
+    elif game.num_players == game.max_players:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game full")
+
+    game.create_player(player_name, username)
+    return game
+
+#START GAME
+@app.put("/game/start/", response_model=Game)
+def start_game(params = Depends(get_game)):
+    game = params["game"]
+    if game.status != 'CREATED':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game already started")
+    elif params["user"].username != game.owner:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only game owner can start the game")
+    elif game.num_players < game.min_players:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not enough players")
+
+    game.start()
+    return game
+
+def get_player(player_name: str, params = Depends(get_game)):
+    game = params["game"]
+    player = game.players.get(player_name)
+    if game.status != 'STARTED':
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Game not started")
+    elif not player:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Player not found")
+    elif not player.is_alive:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Player is not alive")
+    elif player.user_name != params["user"].username:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
+    return {"game": game, "player": player}
+
+#CHOOSE DIRECTOR
+@app.put("/game/elections/nominate/", response_model=Game)
+def choose_director(candidate_name:str, params = Depends(get_player)):
+    game = params["game"]
+    player_name = params["player"].name
+    if candidate_name not in game.players:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Candidate name not found")
+    elif player_name != game.elections.minister_candidate:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only the minister candidate can choose the headmaster candidate")
+    elif candidate_name in [player_name, game.elections.minister, game.elections.headmaster]:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="The candidate is not eligible")
+
+    game.elections.nominate('HEADMASTER', candidate_name)
+    return game
+
+#VOTE
+@app.put("/game/elections/vote/", response_model=Game)
+def vote(vote:Vote, params = Depends(get_player)):
+    game = params["game"]
+    player_name = params["player"].name
+    if not game.elections.headmaster_candidate:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Headmaster candidate not defined")
+    elif player_name in game.elections.votes:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="The player has already voted")
+
+    game.elections.vote(player_name, vote)
+    if game.get_winner():
+        game.finish(manager)
+    return game
+
+#GET PROCLAMATIONS
+@app.get("/game/proclamations/")
+def get_proclamations(params = Depends(get_player)):
+    game = params["game"]
+    if params["player"].name != game.elections.minister:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only minister can get proclamations")
+
+    return game.proclamations.get_proclamations(3)
+
+#DISCARD PROCLAMATION
+@app.put("/game/proclamations/discard/", response_model=Game)
+def discard_proclamation(loyalty: Loyalty, params = Depends(get_player)):
+    game = params["game"]
+    if params["player"].name not in [game.elections.minister, game.elections.headmaster]:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only minister or headmaster can discard a proclamation")
+
+    game.proclamations.discard(loyalty)
+    return game
+
+#ENACT PROCLAMATION
+@app.put("/game/proclamations/enact/", response_model=Game)
+def enact_proclamation(loyalty: Loyalty, params = Depends(get_player)):
+    game = params["game"]
+    if params["player"].name != game.elections.headmaster:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only headmaster can enact a proclamation")
+
+    game.proclamations.enact(loyalty)
+    if game.get_winner():
+        game.finish(manager)
+
+    return game
+
+#CAST SPELL
+@app.put("/game/spells")
+def cast_spell(spell: Spell, target_name: Optional[str] = None, params = Depends(get_player)):
+    game = params ["game"]
+    if target_name and target_name not in game.players:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Target name not found")
+    elif spell not in game.spells:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Spell is not available")
+    elif params["player"].name != game.elections.minister:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only minister can cast a spell")
+
+    result = game.cast_spell(spell, target_name)
+    if game.get_winner():
+        game.finish(manager)
+
+    return result
+
+#LOBBY WEBSOCKET
+@app.websocket("/lobby/")
+async def websocket_lobby(websocket: WebSocket):
+    await manager.connect_lobby(websocket)
+    try:
+        games = [game.dict() for game in manager.games.values() if game.status == 'CREATED']
+        while True:
+            await asyncio.sleep(0.5)
+            await manager.broadcast_json(games, manager.lobby_connections)
+
+    except Exception:
+        manager.disconnect_lobby(websocket)
+
+#GAME WEBSOCKET
+@app.websocket("/game/{game_name}")
+async def websocket_game(websocket: WebSocket, game_name: str):
+    await manager.connect_game(websocket, game_name)
+    try:
+        while True:
+            await asyncio.sleep(0.5)
+            game = manager.games.get(game_name).dict()
+            connections = manager.game_connections.get(game_name)
+            if game and connections:
+                await manager.broadcast_json(game, connections)
+
+    except Exception:
+        manager.disconnect_game(websocket, game_name)
---
+++ Backend/app/server/models.py
@@ -0,0 +1,201 @@
+from typing import Optional, List, Dict, Set
+import random, math
+
+from fastapi import HTTPException, status
+from pydantic import BaseModel, EmailStr
+
+from .enums import *
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    login_id: Optional[str] = None
+
+class User(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    full_name: str = ''
+    disabled: bool = False
+
+class Player(BaseModel):
+    name: str
+    user_name: str
+    is_alive: bool = True
+    role: Role = None
+    loyalty: Loyalty = None
+
+    def kill(self):
+        self.is_alive = False
+
+class Elections(BaseModel):
+    minister_candidate: str = None
+    headmaster_candidate: str = None
+    minister: str = None
+    headmaster: str = None
+    players: Set[str] = None
+    votes: Dict[str, Vote] = {}
+    result: Vote = None
+    rejected: int = 0
+    minister_idx = 0
+
+    def init(self, players: Set[str]):
+        self.players = players
+        self.minister_idx = random.choice(range(len(players)))
+        self.minister_candidate = players[self.minister_idx]
+
+    def nominate(self, nomination: Nomination, candidate: str):
+        if nomination == 'MINISTER':
+            self.minister_candidate = candidate
+        elif nomination == 'HEADMASTER':
+            self.headmaster_candidate = candidate
+
+    def vote(self, player_name: str, vote: Vote):
+        self.votes[player_name] = vote
+        if len(self.votes) == len(self.players):
+            self.set_result()
+
+    def get_result(self):
+        lumos_votes = sum(map(('LUMOS').__eq__, self.votes.values()))
+        nox_votes = sum(map(('NOX').__eq__, self.votes.values()))
+        return 'NOX' if lumos_votes < nox_votes else 'LUMOS'
+
+    def set_result(self):
+        self.result = self.get_result()
+        if self.result == 'NOX':
+            self.rejected += 1
+        else:
+            self.rejected = 0
+            self.minister = self.minister_candidate
+            self.headmaster = self.headmaster_candidate
+
+        self.minister_idx = (self.minister_idx + 1) % len(self.players)
+        self.minister_candidate = self.players[self.minister_idx]
+        self.headmaster_candidate = None
+        self.votes.clear()
+
+class Proclamations(BaseModel):
+    deck: List[Loyalty] = []
+    discarded_proclamations: List[Loyalty] = []
+    PO_enacted_proclamations: int = 0
+    DE_enacted_proclamations: int = 0
+
+    def init(self):
+        for i in range(6):
+            self.deck.append('PHOENIX_ORDER')
+        for i in range(11):
+            self.deck.append('DEATH_EATERS')
+        random.shuffle(self.deck)
+
+    def shuffle(self):
+        if len(self.deck) < 3:
+            self.deck.extend(self.discarded_proclamations)
+            random.shuffle(self.proclamations)
+
+    def get_proclamations(self, num_proclamations: int):
+        self.shuffle()
+        result = []
+        for i in range(num_proclamations):
+            result.append(self.deck.pop())
+        return result
+
+    def enact(self, loyalty: Loyalty):
+        if loyalty == 'PHOENIX_ORDER':
+            self.PO_enacted_proclamations += 1
+        elif loyalty == 'DEATH_EATERS':
+            self.DE_enacted_proclamations += 1
+
+    def discard(self, loyalty: Loyalty):
+        self.discarded_proclamations.append(loyalty)
+
+class Game(BaseModel):
+    name: str
+    password: Optional[str] = None
+    status: GameStatus = 'CREATED'
+    winner: Loyalty = None
+    min_players: int = 5
+    max_players: int = 5
+    num_players: int = 0
+    owner: str = None
+    voldemort: str = None
+    players: Dict[str, Player] = {}
+    proclamations: Proclamations = None
+    elections: Elections = None
+    spells: Set[Spell] = ['ADIVINATION', 'AVADA_KEDAVRA', 'CRUCIO', 'IMPERIUS']
+    chat: List[str] = []
+
+    def exist(self, username: str):
+        users = [player.user_name for player in self.players.values()]
+        return username in users
+
+    def get_username(self, player_name):
+        return self.players[player_name].user_name
+
+    def create_player(self, player_name: str, username: str):
+        self.players[player_name] = Player(name=player_name, user_name=username)
+        self.num_players += 1
+
+    def delete_player(self, player_name: str):
+        self.players.pop(player_name)
+        self.num_players -= 1
+
+    def assign_roles(self):
+        to_assign_phoenix_order = [player for player in self.players.values() if player.loyalty == 'PHOENIX_ORDER']
+        to_assign_death_eaters = [player for player in self.players.values() if player.loyalty == 'DEATH_EATERS']
+        PO_roles = random.sample(PHOENIX_ORDER_ROLES, len(PHOENIX_ORDER_ROLES))
+        DE_roles = random.sample(DEATH_EATERS_ROLES, len(DEATH_EATERS_ROLES))
+        voldemort = to_assign_death_eaters.pop()
+        voldemort.role = 'VOLDEMORT'
+        self.voldemort = voldemort.name
+        while to_assign_phoenix_order:
+            to_assign_phoenix_order.pop().role = PO_roles.pop()
+        while to_assign_death_eaters:
+            to_assign_death_eaters.pop().role = DE_roles.pop()
+
+    def assign_loyalties(self):
+        num_death_eaters = math.ceil(self.num_players / 2)-1
+        num_phoenix_order = self.num_players - num_death_eaters
+        to_assign = random.sample(list(self.players.values()), self.num_players)
+        for i in range(num_phoenix_order):
+            to_assign.pop().loyalty = 'PHOENIX_ORDER'
+        for i in range(num_death_eaters):
+            to_assign.pop().loyalty = 'DEATH_EATERS'
+
+    def start(self):
+        self.status = 'STARTED'
+        self.proclamations = Proclamations()
+        self.proclamations.init()
+        self.assign_loyalties()
+        self.assign_roles()
+        self.elections = Elections()
+        self.elections.init(list(self.players))
+
+    def cast_spell(self, spell: Spell, target: str):
+        self.spells.remove(spell)
+        if spell == 'ADIVINATION':
+            self.proclamations.shuffle()
+            return [self.proclamations.deck[0],
+                    self.proclamations.deck[1],
+                    self.proclamations.deck[2]]
+        elif spell == 'AVADA_KEDAVRA':
+            self.players[target].kill()
+            return self
+
+    def get_winner(self):
+        if self.proclamations.PO_enacted_proclamations == 5:
+            self.winner = 'PHOENIX_ORDER'
+        elif self.proclamations.DE_enacted_proclamations == 6:
+            self.winner = 'DEATH_EATERS'
+        elif not self.players[self.voldemort].is_alive:
+            self.winner = 'PHOENIX_ORDER'
+        elif self.elections.headmaster == self.voldemort and self.proclamations.DE_enacted_proclamations >= 3:
+            self.winner = 'DEATH_EATERS'
+
+        return self.winner
+
+    def finish(self, manager):
+        self.status = 'FINISHED'
+        manager.delete_game(self.name)

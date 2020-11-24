15a34d0dd72d1701c48d72d1aec035695afe2dac
Miguel Angel Rodenas Balestrini <miguel.rodenas@mi.unc.edu.ar>
2020-11-24 01:40:54 -0300

    setting initial files

---
+++ Frontend/src/features/chat/actions/chatAction.js
@@ -0,0 +1,9 @@
+import {
+    SEND_MESSAGE,
+    SEND_MESSAGE_FAIL,
+    SEND_MESSAGE_SUCCESS
+} from '../../../constants/actionTypes/chat';
+
+  import axios from 'axios'
+  import api from '../../../configs/api'
+
---
+++ Frontend/src/features/chat/reducers/chat.js
@@ -0,0 +1,31 @@
+import {
+    
+} from '../../../constants/actionTypes/chat';
+
+const initialState= {
+	status: 'unknow',
+   	errorMsg: '' ,
+}
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case SEND_MESSAGE:
+			return {
+				...state,
+				status: 'loading'
+			}
+		case SEND_MESSAGE_FAIL:
+			return {
+				...state,
+				status: 'failed',
+                errorMsg: action.payload.errorMsg
+			}
+		case SEND_MESSAGE_SUCCESS:
+			return {
+				...state, 
+                status: 'success',
+			}
+		default:
+			return {...state};
+	}
+};

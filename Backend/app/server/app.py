7bbf904dc115f54d35e749dd6c239b3ef51df7f6
Sergio Rodriguez <sergio_mu12@hotmail.com>
2020-11-28 15:19:04 -0300

    Get game

--- Backend/app/server/app.py
+++ Backend/app/server/app.py
@@ -92,6 +92,14 @@ def create_game(game_name: str = Form(..., min_length=5, max_length=20, regex="^
     manager.create_game(game)
     return game
 
+#GET GAME
+@app.get("/game/")
+def get_game(own: bool, user: User = Depends(get_current_active_user)):
+    if own:
+        return [game.name for game in manager.games.values() if game.exist(user.username)]
+    else:
+        return [game for game in manager.games.values() if game.status == 'CREATED']
+
 def get_game(game_name: str, user: User = Depends(get_current_active_user)):
     game = manager.games.get(game_name)
     if not game:

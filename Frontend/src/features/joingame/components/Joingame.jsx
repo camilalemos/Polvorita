4c0f5fd11cd9cdf71ab4cb77ba9223d39e1ee570
Francisco griguol <frangriguol13@gmail.com>
2020-11-29 03:47:39 -0300

    New backgroun

--- Frontend/src/features/joingame/components/Joingame.jsx
+++ Frontend/src/features/joingame/components/Joingame.jsx
@@ -70,52 +70,55 @@ const Joingame = ({ joingame, status, enqueueSnackbar, user, logout, errorMsg })
 	}
 
   return (
-    <div style={{display: 'flex', flexDirection:'column',height: '100%', backgroundImage: `url(${require('../../../constants/images/fondo2.jpeg')})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: "cover"}}>
+    <div style={{display: 'flex', flexDirection:'column',height: '100%', backgroundImage: `url(${require('../../../constants/images/fondo3.jpeg')})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: "cover"}}>
 
 		<div style={{display:'flex',flexDirection:'row', flex:1}}>
 			<div style={{ flex:1 }} />
-			<div  style={{display:'flex',flexDirection:'column', flex:3, height:'90%'}}>
-				<div style={{ display: 'flex', flexDirection:'row', justifyContent:'flex-end', alignItems:'center' , paddingTop:40, paddingBottom:40}}>
+			<div  style={{display:'flex',flexDirection:'column', flex:3, justifyContent:'center' }}>
+				<div  style={{display:'flex', height:'70%'}}>   
+					<Grid component={Paper} style={{padding:40, flex:3, opacity:0.8, overflow:'auto', backgroundColor:'black'}}>
+						<div style={{display:'flex', marginBottom: 40}}>
+							<a style={{flex: 1, fontSize:30, color:'white'}}>Game name</a>
+							<a style={{flex: 1, textAlign: 'center', fontSize:30, color:'white'}}>Owner</a>
+							<a style={{flex: 1, textAlign: 'center', fontSize:30, color:'white'}}>Num Players</a>
+							<a style={{flex: 1, textAlign: 'center', fontSize:30, color:'white'}}></a>
+						</div>
+						{gameInfo.map(currentGame => {
+							
+							return (
+							<div key={currentGame.name}>
+								<div style={{display:'flex',alignItems:'center', cursor:'pointer', flex:1}} onClick={() => history.push(`/lobby/${currentGame.name}`)}>
+									<a style={{flex: 1, fontSize: 20, color:'white'}}>{currentGame.name}</a>
+									<a style={{flex: 1, textAlign: 'center', fontSize: 20, color:'white'}}>{currentGame.owner}</a>
+									<a style={{flex: 1, textAlign: 'center', fontSize: 20, color:'white'}}>{currentGame.num_players}/{currentGame.max_players}</a>
+									<div style={{flex: 1, textAlign: 'right', display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
+									<ListItemIcon >
+										{currentGame.password === null ?
+										<LockOpenIcon color='secondary'/> : <LockIcon/>} 
+									</ListItemIcon>
+									{isJoing(currentGame) && currentGame.num_players < currentGame.max_players && <Button onClick={(e) => handleJoinNewGame(e)} variant="contained" >Join Game</Button>}
+									</div>
+								</div>
+								<div style={{  height:.5 , backgroundColor:'lightgrey', display:'flex', marginBottom:20, marginTop:10}} />
+								<PopUp join={() => handleJoin(currentGame.name ,playerName, gamePassword)} open={openModal} playerName={playerName} gamePassword={gamePassword} setPlayerName={(value) => setPlayerName(value)} setPassword={(value) => setPassword(value)} onClose={() => setOpenModal(false)}/>
+							</div>	
+						)})
+						}
+					</Grid>
+				</div>
+			</div>
+			<div style={{ flex:1, display:'flex', justifyContent:'center', alignItems:'flex-start' }}> 
+			<div style={{ display: 'flex', flexDirection:'row', justifyContent:'flex-end', alignItems:'center' , paddingTop:40, paddingBottom:40}}>
 					<div>
 						{/* <Button style={{ backgroundColor:'lightblue', marginRight:20, backgroundColor:'black' }} size='small' color='primary' onClick={() => setOpenModalChangeProfile(true)} variant="contained" >Profile</Button>
 						<Button style={{ backgroundColor:'lightblue', marginRight:20, backgroundColor:'black' }} size='small' color='primary' onClick={() => setOpenModalCreateGame(true)} variant="contained" >Create Game</Button>
 						<Button style={{ backgroundColor:'lightblue', backgroundColor:'black' }} size='small' onClick={handleLogout} color='primary' variant="contained" >Logout</Button> */}
-						<IconButton size='large' onClick={() => setOpenModalChangeProfile(true)}> <AccountBoxOutlinedIcon fontSize='large'/> </IconButton>
-						<IconButton size='large' onClick={() => setOpenModalCreateGame(true)}> <AddBoxOutlinedIcon fontSize='large'/> </IconButton>
-						<IconButton size='large' onClick={handleLogout}> <ExitToAppOutlinedIcon fontSize='large'/> </IconButton>
+						<IconButton size='large' onClick={() => setOpenModalChangeProfile(true)}> <AccountBoxOutlinedIcon color='secondary' fontSize='large'/> </IconButton>
+						<IconButton size='large' onClick={() => setOpenModalCreateGame(true)}> <AddBoxOutlinedIcon color='secondary' fontSize='large'/> </IconButton>
+						<IconButton size='large' onClick={handleLogout}> <ExitToAppOutlinedIcon color='secondary' fontSize='large'/> </IconButton>
 					</div>
-				</div>
-				<Grid component={Paper} style={{padding:40, flex:3, opacity:0.8, overflow:'auto', backgroundColor:'black'}}>
-					<div style={{display:'flex', marginBottom: 40}}>
-						<a style={{flex: 1, fontSize:30, color:'white'}}>Game name</a>
-						<a style={{flex: 1, textAlign: 'center', fontSize:30, color:'white'}}>Owner</a>
-						<a style={{flex: 1, textAlign: 'center', fontSize:30, color:'white'}}>Num Players</a>
-						<a style={{flex: 1, textAlign: 'center', fontSize:30, color:'white'}}></a>
-					</div>
-					{gameInfo.map(currentGame => {
-						
-						return (
-						<div key={currentGame.name}>
-							<div style={{display:'flex',alignItems:'center', cursor:'pointer', flex:1}} onClick={() => history.push(`/lobby/${currentGame.name}`)}>
-								<a style={{flex: 1, fontSize: 20, color:'white'}}>{currentGame.name}</a>
-								<a style={{flex: 1, textAlign: 'center', fontSize: 20, color:'white'}}>{currentGame.owner}</a>
-								<a style={{flex: 1, textAlign: 'center', fontSize: 20, color:'white'}}>{currentGame.num_players}/{currentGame.max_players}</a>
-								<div style={{flex: 1, textAlign: 'right', display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
-								<ListItemIcon >
-									{currentGame.password === null ?
-									<LockOpenIcon color='secondary'/> : <LockIcon/>} 
-								</ListItemIcon>
-								{isJoing(currentGame) && currentGame.num_players < currentGame.max_players && <Button onClick={(e) => handleJoinNewGame(e)} variant="contained" >Join Game</Button>}
-								</div>
-							</div>
-							<div style={{  height:.5 , backgroundColor:'lightgrey', display:'flex', marginBottom:20, marginTop:10}} />
-							<PopUp join={() => handleJoin(currentGame.name ,playerName, gamePassword)} open={openModal} playerName={playerName} gamePassword={gamePassword} setPlayerName={(value) => setPlayerName(value)} setPassword={(value) => setPassword(value)} onClose={() => setOpenModal(false)}/>
-						</div>	
-					)})
-					}
-				</Grid>
+				</div>	
 			</div>
-			<div style={{ flex:1 }} />
 		</div>
 		<ChangeProfileContainer open={openModalChangeProfile} onClose={() => setOpenModalChangeProfile(false)}/>
 		<CreateGameContainer open={openModalCreateGame} onClose={() => setOpenModalCreateGame(false)}/>

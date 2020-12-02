caa4453d4d108a2e7dfecce341ba999e338f68ff
Tomás Uribe <tomas.uribe@mi.unc.edu.ar>
2020-12-01 22:19:23 -0300

    Fix More styles

--- Frontend/src/features/showRole/components/ShowRole.jsx
+++ Frontend/src/features/showRole/components/ShowRole.jsx
@@ -100,8 +100,8 @@ const ShowRole = ({ user, gameInfo }) => {
 
         <div style={{ display: 'flex', flexDirection: 'column', padding: 20 }}>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                <img src={handelSelectImgRole(userRole, userLoyalty)} alt="Role" style={{ display: 'flex', width: '200px', height: '270', padding: 5 }} />
-                <img src={handleSelectImgLoyalty(userLoyalty)} alt="Loyalty" style={{ display: 'flex', width: '200px', height: '270', padding: 5 }} />
+                <img src={handelSelectImgRole(userRole, userLoyalty)} alt="Role" style={{ display: 'flex', width: '150px', height: '270', padding: 5 }} />
+                <img src={handleSelectImgLoyalty(userLoyalty)} alt="Loyalty" style={{ display: 'flex', width: '150px', height: '270', padding: 5 }} />
             </div>
         </div>
     );
